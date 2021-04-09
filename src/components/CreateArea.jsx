d34a49d69af5adf201d253abc2838eaeb93f8ae2
prabhu yadav p.s <prabhuyadav007.p.y@gmail.com>
2021-04-09 09:55:05 +0530

    Added some more style components and a background texture from transparenttextures.com

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
   const [note, setNote] = useState({ title: "", content: "" });
   function handleChange(event) {
     const { name, value } = event.target;
@@ -16,25 +18,32 @@ function CreateArea(props) {
     setNote({ title: "", content: "" });
   }
 
+  const expand = () => setExpanded(true);
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          placeholder="Title"
-          value={note.title}
-          onChange={handleChange}
-        />
+        {isExpanded && (
+          <input
+            name="title"
+            placeholder="Title"
+            value={note.title}
+            onChange={handleChange}
+          />
+        )}
         <textarea
           name="content"
           placeholder="Take a note..."
           value={note.content}
-          rows="3"
+          rows={isExpanded ? "3" : "1"}
           onChange={handleChange}
+          onClick={expand}
         />
-        <Fab type="submit" onClick={add}>
-          <AddIcon />
-        </Fab>
+        <Zoom in={isExpanded}>
+          <Fab type="submit" onClick={add}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
